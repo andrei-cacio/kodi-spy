969d6584a249968629580384deefe70fb4a6622a
Andrei Cacio <andrei.cacio@yahoo.com>
2015-08-21 10:06:47 +0300

    Refactored the code

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-var chokidar = require('chokidar'),
-    request = require('request'),
+var Spy = require('./lib/spy'),
+    CommandDispatcher = require('./lib/core/command-dispatcher'),
+    Command = require('./lib/core/command'),
     folderDest = process.argv[2],
-    kodiHost = process.argv[3],
-    watcher = chokidar.watch(folderDest, {
-      ignored: /[\/\\]\./, persistent: true
-    }),
-    exec = require('child_process').exec;
+    kodiHost = process.argv[3];
 
-var log = console.log.bind(console);
-log('Watching folder: ' + folderDest);
 
-watcher
-    .on('ready', function(path) {
-        watcher.on('add', function(path) {
-            if (path.search('.part') === -1) {
-                log('File', path, 'has been added');
-                exec('notify-send "Kodi", "Updating video library"');
-                request('http://' + kodiHost + '/jsonrpc?request={"jsonrpc":"2.0","method":"VideoLibrary.Scan"}');
-            }
-        });
-    });
+var scanCommand = new Command(Command.TYPE.SCAN),
+    commandDispatcher = new CommandDispatcher(kodiHost, scanCommand),
+    spy = new Spy(folderDest, commandDispatcher);
+
+spy.watch();
